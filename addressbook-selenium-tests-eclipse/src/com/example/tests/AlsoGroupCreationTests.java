7093c395c231489732f4ccfca701af97bce897ab
juliamanunia <julia.manunia@yahoo.com>
2014-10-12 21:27:39 +0300

    Add random method

--- addressbook-selenium-tests-eclipse/src/com/example/tests/AlsoGroupCreationTests.java
+++ addressbook-selenium-tests-eclipse/src/com/example/tests/AlsoGroupCreationTests.java
@@ -10,9 +10,9 @@ public class AlsoGroupCreationTests extends TestBase{
     gotoGroupsPage();
     initGroupCreation();
     GroupData group = new GroupData();
-    group.name = "group name 1";
-    group.header = "header 1";
-    group.footer = "footer 1";
+    group.name = "group name " + myRandom(1);
+    group.header = "header " + myRandom(1);
+    group.footer = "footer " + myRandom(1);
 	fillGroupForm(group);
     submitGroupCreation();
     returnToGroupsPage();
--- addressbook-selenium-tests-eclipse/src/com/example/tests/TestBase.java
+++ addressbook-selenium-tests-eclipse/src/com/example/tests/TestBase.java
@@ -2,6 +2,7 @@ package com.example.tests;
 
 import static org.junit.Assert.fail;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.Alert;
@@ -142,6 +143,45 @@ public class TestBase {
 
 	protected void initContactCreation() {
 		driver.findElement(By.linkText("add new")).click();
+	}
+	
+	protected String myRandom(int a){
+		Random random = new Random();
+		String sRand = null;
+		//Random for simple numbers
+		if (a == 1){
+			int rand = random.nextInt(1000) + 1;
+			sRand = Integer.toString(rand);
+		}
+		//Random for bYear
+		else if(a == 2){
+			int rand = 1920 + (int)(Math.random() * ((2014 - 1920) + 1));
+			sRand = Integer.toString(rand);
+		}
+		//Random for bMonth
+		else if(a == 3){
+			int rand = random.nextInt(13) + 1;
+            switch (rand){
+                case 1: sRand = "January"; break;
+                case 2: sRand = "February"; break;
+                case 3: sRand = "March"; break;
+                case 4: sRand = "April"; break;
+                case 5: sRand = "May"; break;
+                case 6: sRand = "June"; break;
+                case 7: sRand = "July"; break;
+                case 8: sRand = "August"; break;
+                case 9: sRand = "September"; break;
+                case 10: sRand = "October"; break;
+                case 11: sRand = "November"; break;
+                case 12: sRand = "December"; break;
+            }
+		}
+		//Random for bDay
+		else if(a == 4){
+			int rand = random.nextInt(29) + 1;
+			sRand = Integer.toString(rand);
+		}
+		return sRand;
 	}
 
 }
